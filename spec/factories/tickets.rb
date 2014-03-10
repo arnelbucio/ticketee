c214fc5130c86888b7cba9e3bafd13919195b2ea
Arnel Bucio <rnlbucio@gmail.com>
2014-03-10 12:49:42 +0800

    Implement displaying a list of relevant tickets.

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,9 +1,7 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :ticket do
-    title "MyString"
-    description "MyText"
-    project nil
+    title "Example ticket"
+    description "An example ticket."
+    # project nil
   end
 end
