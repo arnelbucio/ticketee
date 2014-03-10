85248507260fd14fe11843b308a9f2cb5f279bc3
Arnel Bucio <rnlbucio@gmail.com>
2014-03-10 21:11:41 +0800

    Associate users to tickets when tickets are created.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name "Arnel"
-    email "arnel@example.com"
+    name "example_user"
+    email "sample@example.com"
     password "password"
     password_confirmation "password"
   end
