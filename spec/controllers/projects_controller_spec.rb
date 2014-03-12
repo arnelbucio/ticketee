9d8b11d4a62552b21eded64b759556aa2f41f6de
Arnel Bucio <rnlbucio@gmail.com>
2014-03-12 21:16:42 +0800

    Make projects only visible to users with permission to see them

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,6 +3,20 @@ require 'spec_helper'
 describe ProjectsController do
   let(:user) { FactoryGirl.create :user }
   let(:project) { FactoryGirl.create :project }
+
+  before do
+    sign_in(user)
+  end
+
+  it "displays an error for a missing project" do
+    get :show, id: "not-here"
+
+    expect(response).to redirect_to(projects_path)
+    message = "The project you were looking for could not be found."
+
+    expect(flash[:alert]).to eql(message)
+  end
+
   context "standard users" do
     { new: :get,
       create: :post,
@@ -15,6 +29,13 @@ describe ProjectsController do
         expect(response).to redirect_to('/')
         expect(flash[:alert]).to eql("You must be an admin to do that.")
       end
+    end
+
+    it "cannot access the show action without permission" do
+      get :show, id: project.id
+
+      expect(response).to redirect_to(projects_path)
+      expect(flash[:alert]).to eql("The project you were looking for could not be found.")
     end
   end
 end
