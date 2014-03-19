470f8ff0e737aafed204ca946f324725b8e2db44
Arnel Bucio <rnlbucio@gmail.com>
2014-03-19 20:54:06 +0800

    Add permissions screen for admins

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Ticketee::Application.routes.draw do
 
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
+  delete "signout", to: "sessions#destroy", as: "signout"
 
   namespace :admin do
     root 'base#index'
-    resources :users
+    resources :users do
+      resources :permissions
+      put "permissions", to: "permissions#set",
+                         as: "set_permissions"
+    end
   end
 
   resources :projects do
