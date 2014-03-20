cc10cde47244d40276621a2bd68ca3e035b08297
Arnel Bucio <rnlbucio@gmail.com>
2014-03-20 19:20:05 +0800

    Users can now leave comments on tickets

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  before_filter :require_signin!
+  before_filter :set_ticket
+
+  def create
+    @comment = @ticket.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      flash[:notice] = "Comment has been created."
+      redirect_to [@ticket.project, @ticket]
+    else
+      flash[:alert] = "Comment has not been created."
+      render "tickets/show"
+    end
+  end
+
+  private
+    def set_ticket
+      @ticket = Ticket.find(params[:ticket_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Ticketee::Application.routes.draw do
     resources :tickets
   end
 
+  resources :tickets do
+    resources :comments
+  end
+
   resources :files
 
   resources :users
